2971f5a2e607fcf0b3fde199494934ed7f2f9401
infreeJ <kasious97@naver.com>
2025-07-31 19:30:24 +0900

    댓글 작성 servlet 대략적으로 만듦

--- 404_mid_project/src/main/java/controller/noti/NotiCommentServlet.java
+++ 404_mid_project/src/main/java/controller/noti/NotiCommentServlet.java
@@ -1,16 +1,64 @@
 package controller.noti;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.noti.NotificationDao;
 import model.noti.NotificationDto;
 
-@WebServlet("")
-public class NotiCommentServlet {
-	NotificationDto notiDto = new NotificationDto();
-	
-	Long notiRecipientNum = notiDto.getNotiRecipientNum();
-	Long notiSenderNum = notiDto.getNotiSenderNum();
-	int notiTypeCode = notiDto.getNotiTypeCode();
-	int notiTargetTypeCode = notiDto.getNotiTargetTypeCode();
-	Long notiTargetNum = notiDto.getNotiTargetNum();
-	String notiMessage = notiDto.getNotiMessage();
+
+
+@WebServlet("*/noti")
+public class NotiCommentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 사용자 요청 path 추출
+		String uri = req.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/"));
+		
+		boolean isCommentSuccess = true; // 댓글 서블릿과 합치기 전 임시 변수
+		
+		if(isCommentSuccess) {
+			
+		
+			
+		Long notiRecipientNum = Long.valueOf(req.getParameter("notiRecipientNum"));
+		Long notiSenderNum = Long.valueOf(req.getParameter("notiSenderNum"));
+		int notiTypeCode = Integer.parseInt(req.getParameter("notiTypeCode"));
+		int notiTargetTypeCode = Integer.parseInt(req.getParameter("notiTargetTypeCode"));
+		Long notiTargetNum = Long.valueOf(req.getParameter("notiTargetNum"));
+		String notiMessage = req.getParameter("notiMessage");
+		
+
+		NotificationDto notiDto = new NotificationDto();
+		
+		notiDto.setNotiRecipientNum(notiRecipientNum);
+		notiDto.setNotiSenderNum(notiSenderNum);
+		notiDto.setNotiTypeCode(notiTypeCode);
+		notiDto.setNotiTargetTypeCode(notiTargetTypeCode);
+		notiDto.setNotiTargetNum(notiTargetNum);
+		notiDto.setNotiMessage(notiMessage);
+		
+		boolean isNotiSuccess = NotificationDao.getInstance().commentInsert(notiDto);
+		
+		if(isNotiSuccess) {
+			System.out.println("알림 데이터 저장 성공");
+			resp.sendRedirect("리디렉션 경로");
+		} else {
+			System.out.println("알림 데이터 저장 실패");
+			resp.sendRedirect("리디렉션 경로");
+		}
+		
+		}
+	}
 }
+
+
+
+
