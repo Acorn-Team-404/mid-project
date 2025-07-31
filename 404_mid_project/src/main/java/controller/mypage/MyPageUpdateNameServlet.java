a1477eab5ab5b0926d21fa4e54ce6337c7391189
youngseok <gys329@naver.com>
2025-07-31 18:10:05 +0900

    마이페이지 이름, 전화번호 변경하기

---
+++ 404_mid_project/src/main/java/controller/mypage/MyPageUpdateNameServlet.java
@@ -0,0 +1,38 @@
+package controller.mypage;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.user.UserDao;
+import model.user.UserDto;
+
+@WebServlet("/update-name")
+public class MyPageUpdateNameServlet extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 세션에 로그인 된 사용자 ID 가져오기
+		String usersId = (String) req.getSession().getAttribute("usersId");
+		
+		
+		// 새 이름 값 받기
+		String newName = req.getParameter("usersName");
+		
+		// dto 값 담기
+		UserDto user = new UserDto();
+		System.out.println("변경 요청된 이름: " + newName);
+		user.setUsersId(usersId);
+		user.setUsersName(newName);
+		
+		boolean result = UserDao.getInstance().updateName(user);
+		
+		// redirect my-page로
+	  resp.sendRedirect(req.getContextPath() + "/my-page"); 
+
+	}
+}
---
+++ 404_mid_project/src/main/webapp/my-page/update-phone.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@page import="model.user.UserDto"%>
+    <%
+    UserDto user = (UserDto) session.getAttribute("user");
+    %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>/my-page/update-phone.jsp</title>
+</head>
+<body>
+	<h2>전화번호 수정</h2>
+	<form action="${pageContext.request.contextPath}/update-phone" method="post">
+	<label>새 전화번호 : </label>
+	<input type="text" name="phone" value = "<%= user != null ? user.getPhone() : ""%>" />
+	<button type="submit">수정하기</button>
+	</form>
+</body>
+</html>
