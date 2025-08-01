77268fcab9b114ef3c1986bff0be5e296cf9c512
kimchaeryn <ryn3439@naver.com>
2025-08-01 17:23:45 +0900

    jincairin

--- 404_mid_project/src/main/java/controller/book/BookSaveServlet.java
+++ 404_mid_project/src/main/java/controller/book/BookSaveServlet.java
@@ -1,6 +1,8 @@
 package controller.book;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -32,6 +34,13 @@ public class BookSaveServlet extends HttpServlet {
 			 req.setAttribute("phone", user.getUsersPhone());
 		 }
 		 req.getRequestDispatcher("/booking/booking-form.jsp").forward(req, res);
+		 
+		 // 숙소 정보 가져오기
+		 /* int stayNum = 세션이나 파라미터로 받기
+		 List<RoomDto> roomList = RoomDao.getInstance().getRoomsByStayNum(stayNum);
+
+		 req.setAttribute("roomList", roomList);
+		 req.setAttribute("bookStayNum", stayNum);*/
 	 }
 	
 	@Override
@@ -44,42 +53,77 @@ public class BookSaveServlet extends HttpServlet {
 		// DTO 세팅
 		BookDto dto = new BookDto();
 		
+		dto.setBookUsersNum(usersNum);
+		
 		// 예약 번호
 		String bookNum = BookDao.getInstance().generateBookNum();		
 		dto.setBookNum(bookNum);
 		
-		int roomId = Integer.parseInt(req.getParameter("bookRoomId"));
-		
-		// 숙소 값도 해야함
+		// 숙소 번호
+		int stayNum = Integer.parseInt(req.getParameter("bookStayNum"));
+		dto.setBookStayNum(stayNum);
+		// 객실 번호
+		int roomNum = Integer.parseInt(req.getParameter("bookRoomNum"));
+		dto.setBookRoomNum(roomNum);
 		
 		String checkIn = req.getParameter("checkIn");
+		dto.setBookCheckIn(checkIn);
 		String checkOut =  req.getParameter("checkOut");
+		dto.setBookCheckOut(checkOut);
+		
+		LocalDate checkInDate = LocalDate.parse(checkIn);
+		LocalDate checkOutDate = LocalDate.parse(checkOut);
+		
+		long betweenDay = ChronoUnit.DAYS.between(checkInDate, checkOutDate);
+		if(betweenDay <= 0) {
+			// 체크인 날짜는 체크아웃 날짜보다 이전이어야 한다
+			req.setAttribute("errorMsg", "체크아웃 날짜는 체크인 날짜보다 늦어야 합니다");
+			return;
+		}
 		
 		int adult = Integer.parseInt(req.getParameter("adult"));
+		dto.setBookAdult(adult);
 		int children = Integer.parseInt(req.getParameter("children"));
+		dto.setBookChildren(children);
 		int infant = Integer.parseInt(req.getParameter("infant"));
+		dto.setBookInfant(infant);
+		// 총 인원
 		int totalPax = adult + children + infant;
+		dto.setBookTotalPax(totalPax);
 		
-		String bedParam = req.getParameter("bed");
-		int extraBed = 0;
-		int infantBed = 0;
-		
+		// 추가 침대 옵션 : 기본은 0, 선택하는 경우 1
 		String[] beds = req.getParameterValues("bed");
+		int extraBed = 0;
+		int infantBed = 0;	
 		if(beds != null) {
 			for(String bed : beds) {
 				if("extraBed".equals(bed)) extraBed = 1;
 				if("infantBed".equals(bed)) infantBed = 1;
 			}
 		}
+		dto.setBookExtraBed(extraBed);
+		dto.setBookInfantBed(infantBed);
 		
 		String checkInTime = req.getParameter("checkInTime");
-		
 		String bookRequest = req.getParameter("bookRequest");
-
-		int totalAmount = 0;
+		dto.setBookCheckInTime(checkInTime);
+		dto.setBookRequest(bookRequest);
+		
+		int roomPrice = BookDao.getInstance().getRoomPrice(roomNum);
+		
+		int totalAmount =  (int) (roomPrice * betweenDay);
+		dto.setBookTotalAmount(totalAmount);
 		
 		// 예약 상태 (예약 대기)
-		int bookStatusCode = 10;
+		dto.setBookStatusCode(10);
+		
+		boolean isSuccess = BookDao.getInstance().insert(dto);
+		
+		if(!isSuccess) {
+			req.setAttribute("errorMsg", "예약 실패했습니다.");
+	        req.getRequestDispatcher("/booking/booking-form.jsp").forward(req, res);
+	        return;
+		} 
 	
 	}
 }
