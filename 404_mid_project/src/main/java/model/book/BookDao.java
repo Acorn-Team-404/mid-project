c6818160c3b8b6da045a20b2a24a50c0e064c93a
danjae1 <numbness98@naver.com>
2025-08-11 10:40:49 +0900

    날짜겹침방지 + 예약대기 상태 제거 리스너 추가

--- 404_mid_project/src/main/java/model/book/BookDao.java
+++ 404_mid_project/src/main/java/model/book/BookDao.java
@@ -35,6 +35,45 @@ public class BookDao {
       return dao;
    }
    
+   // BookDao.java
+   public boolean isDateOverlap(BookDto dto) {
+	   	Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+	   String sql = """
+	   		    SELECT 1
+	   		    FROM BOOKING
+	   		    WHERE BOOK_ROOM_NUM = ?
+	   		      AND BOOK_STATUS_CODE IN (10,11)
+	   		      AND (? < TO_DATE(BOOK_CHECKOUT_DATE, 'YYYY-MM-DD')
+	   		      AND ? > TO_DATE(BOOK_CHECKIN_DATE, 'YYYY-MM-DD'))
+	   		""";
+	   
+	   try {
+			 
+		   
+		   conn = DBConnector.getConn();
+		   pstmt = conn.prepareStatement(sql);
+		   pstmt.setLong(1, dto.getBookRoomNum());
+		   pstmt.setString(2, dto.getBookCheckIn()); // 새 체크아웃 < 기존 체크인보다 이후
+		   pstmt.setString(3, dto.getBookCheckOut());  // 새 체크인 > 기존 체크아웃보다 이전
+		   rs = pstmt.executeQuery();
+		   
+		   
+		   if(rs.next()) {
+			   return true;
+		   }
+		   
+		   return false;
+		   
+	   } catch (Exception e) {
+		   e.printStackTrace();
+		   return true; // 예외 발생 시 그냥 겹친다 처리 
+	   } finally {
+		   DBConnector.close(rs, pstmt, conn);
+	   }
+   }
+   
    // 예약대기 상태로 오래있는 데이터 삭제시키는 DAO
    public List<ExpiredBookingDto> findExpiredBook(Connection conn) throws SQLException {
 	    String sql = """
